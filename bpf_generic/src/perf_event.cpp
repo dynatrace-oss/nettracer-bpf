a0fce5671fe2a8fcf2cb737abc0d80e156937fd7
pawsten <pawsten@gmail.com>
2022-02-08 15:39:58 +0100

    Memory leak fix

--- bpf_generic/src/perf_event.cpp
+++ bpf_generic/src/perf_event.cpp
@@ -14,8 +14,15 @@
 #include <variant>
 #include <vector>
 
+template <typename... Ts>
+struct overload : Ts... {
+	using Ts::operator()...;
+};
+template <class... Ts>
+overload(Ts...) -> overload<Ts...>;
+
 template <typename T>
-using event = std::variant<perf_event_sample<T>*, perf_event_lost*, std::nullptr_t>;
+using event = std::variant<perf_event_sample<T>, perf_event_lost, std::monostate>;
 
 // pattern https://github.com/cilium/cilium/blob/master/pkg/bpf/perf.go
 template <typename T>
@@ -31,15 +38,15 @@ event<T> perf_event_read(int page_count, int page_size, read_state* state, perf_
 	// No data to read on this ring
 	__sync_synchronize();
 	if (data_head == data_tail)
-		return nullptr;
+		return std::monostate{};
 
 	begin = base + data_tail % raw_size;
 	e = (perf_event_sample<T>*)begin;
 	end = base + (data_tail + e->header.size) % raw_size;
 
 	if (state->buf_len < e->header.size || !state->buf) {
-		state->buf = realloc(state->buf, e->header.size);
-		state->buf_len = e->header.size;
+		LOG_INFO("event of unexpected size {}", e->header.size );
+		return std::monostate{};
 	}
 
 	if (end < begin) {
@@ -50,13 +57,12 @@ event<T> perf_event_read(int page_count, int page_size, read_state* state, perf_
 	} else {
 		memcpy(state->buf, begin, e->header.size);
 	}
-
 	switch (e->header.type) {
 	case PERF_RECORD_SAMPLE:
-		evt = (perf_event_sample<T>*)state->buf;
+		evt =  *((perf_event_sample<T>*)state->buf);
 		break;
 	case PERF_RECORD_LOST:
-		evt = (perf_event_lost*)state->buf;
+		evt  = *((perf_event_lost*)state->buf);
 		break;
 	}
 
@@ -91,22 +97,23 @@ void event_reader::read_loop(Func f) {
 		static std::vector<T> events(pc);
 		events.clear();
 		for (int cpu = 0; cpu < pc; cpu++) {
-			bool is_result = false;
-			do {
+			bool is_result = true;
+			while (is_result) {
 				auto res = perf_event_read<T>(perf_data.page_count, page_size, &rstate, perf_data.header[cpu]);
-				is_result = !std::holds_alternative<std::nullptr_t>(res);
-				if (is_result) {
-					try {
-						perf_event_sample<T>* smple = std::get<0>(res);
-						T* evt = (T*)&(smple->data);
-						events.emplace_back(*evt);
-					} catch (const std::bad_variant_access&) { // PERF_RECORD_LOST:
-						LOG_DEBUG("event lost");
-						break;
-					}
-				}
-
-			} while (is_result);
+				is_result = std::visit(
+						overload{
+								[&](perf_event_sample<T>& sample) {
+									T* evt = (T*)&(sample.data);
+									events.emplace_back(*evt);
+									return true;
+								},
+								[](perf_event_lost) {
+									LOG_DEBUG("event lost");
+									return false;
+								},
+								[](std::monostate) { return false; }},
+						res);
+			}
 		}
 
 		std::sort(events.begin(), events.end(), [](T const& a, T const& b) { return a.timestamp < b.timestamp; });
