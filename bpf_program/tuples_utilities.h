e796ec7d39da3ed91145de2c07382c7b551f3b00
Pawel Stenka <pawel.stenka@dynatrace.com>
2026-04-29 11:04:48 +0200

    Reading namespace moved to function

--- bpf_program/tuples_utilities.h
+++ bpf_program/tuples_utilities.h
@@ -39,6 +39,24 @@ enum protocol {
 };
 
 __attribute__((always_inline))
+static uint32_t get_ns(struct guess_status_t *status, struct sock *sk){
+	uint32_t netns = 0;
+#ifdef LEGACY_BPF
+	possible_net_t *skc_net = NULL;
+	bpf_probe_read(&skc_net, sizeof(void *), ((char *)sk) + status->offset_netns);
+	bpf_probe_read(&netns, sizeof(netns), ((char *)skc_net) + status->offset_ino);
+#else
+	struct net *net_ptr = NULL;
+	bpf_core_read(&net_ptr, sizeof(net_ptr), &sk->__sk_common.skc_net.net);
+	if (net_ptr) {
+		bpf_core_read(&netns, sizeof(netns), &net_ptr->ns.inum);
+	}
+#endif
+	return netns;
+}
+
+
+__attribute__((always_inline))
 static bool check_family(struct sock *sk, uint16_t expected_family) {
 	struct guess_status_t *status;
 	uint32_t zero = 0;
@@ -62,33 +80,25 @@ static bool check_family(struct sock *sk, uint16_t expected_family) {
 __attribute__((always_inline))
 static int read_ipv4_tuple(struct ipv4_tuple_t *tuple, struct guess_status_t *status, struct sock *sk)
 {
-	uint32_t saddr = 0, daddr = 0, net_ns_inum = 0;
+	uint32_t saddr = 0, daddr = 0;
 	uint16_t sport = 0, dport = 0;
 
 #ifdef LEGACY_BPF
-	possible_net_t *skc_net = NULL;
 	bpf_probe_read(&saddr, sizeof(saddr), ((char *)sk) + status->offset_saddr);
 	bpf_probe_read(&daddr, sizeof(daddr), ((char *)sk) + status->offset_daddr);
 	bpf_probe_read(&sport, sizeof(sport), ((char *)sk) + status->offset_sport);
 	bpf_probe_read(&dport, sizeof(dport), ((char *)sk) + status->offset_dport);
-	bpf_probe_read(&skc_net, sizeof(void *), ((char *)sk) + status->offset_netns);
-	bpf_probe_read(&net_ns_inum, sizeof(net_ns_inum), ((char *)skc_net) + status->offset_ino);
 #else
 	bpf_core_read(&saddr, sizeof(saddr), &sk->__sk_common.skc_rcv_saddr);
 	bpf_core_read(&daddr, sizeof(daddr), &sk->__sk_common.skc_daddr);
 	bpf_core_read(&sport, sizeof(sport), &sk->__sk_common.skc_num);
 	bpf_core_read(&dport, sizeof(dport), &sk->__sk_common.skc_dport);
-	struct net *net_ptr = NULL;
-	bpf_core_read(&net_ptr, sizeof(net_ptr), &sk->__sk_common.skc_net.net);
-	if (net_ptr) {
-		bpf_core_read(&net_ns_inum, sizeof(net_ns_inum), &net_ptr->ns.inum);
-	}
 #endif
 	tuple->saddr = saddr;
 	tuple->daddr = daddr;
 	tuple->sport = sport;
 	tuple->dport = ntohs(dport);
-	tuple->netns = net_ns_inum;
+	tuple->netns = get_ns(status, sk);
 
 	// if addresses or ports are 0, ignore
 	if (saddr == 0 || daddr == 0 ||  dport == 0 || sport == 0 ) {
@@ -102,19 +112,15 @@ __attribute__((always_inline))
 static int read_ipv6_tuple(struct ipv6_tuple_t *tuple, struct guess_status_t *status, struct sock *sk)
 {
 	uint64_t saddr_h = 0, saddr_l = 0, daddr_h = 0, daddr_l = 0;
-	uint32_t net_ns_inum = 0;
 	uint16_t sport = 0, dport = 0;
 
 #ifdef LEGACY_BPF
-	possible_net_t *skc_net = NULL;
 	bpf_probe_read(&saddr_h, sizeof(saddr_h), ((char *)sk) + status->offset_daddr_ipv6 + 2 * sizeof(uint64_t));
 	bpf_probe_read(&saddr_l, sizeof(saddr_l), ((char *)sk) + status->offset_daddr_ipv6 + 3 * sizeof(uint64_t));
 	bpf_probe_read(&daddr_h, sizeof(daddr_h), ((char *)sk) + status->offset_daddr_ipv6);
 	bpf_probe_read(&daddr_l, sizeof(daddr_l), ((char *)sk) + status->offset_daddr_ipv6 + sizeof(uint64_t));
 	bpf_probe_read(&sport, sizeof(sport), ((char *)sk) + status->offset_sport);
 	bpf_probe_read(&dport, sizeof(dport), ((char *)sk) + status->offset_dport);
-	bpf_probe_read(&skc_net, sizeof(void *), ((char *)sk) + status->offset_netns);
-	bpf_probe_read(&net_ns_inum, sizeof(net_ns_inum), ((char *)skc_net) + status->offset_ino);
 #else
 	struct inet_sock *inet = (struct inet_sock *)sk;
     struct ipv6_pinfo *np = inet->pinet6;
@@ -130,11 +136,6 @@ static int read_ipv6_tuple(struct ipv6_tuple_t *tuple, struct guess_status_t *st
      //           struct rt6_info *rt = (struct rt6_info *)dst;
      //           struct in6_addr daddr = {};
      //           bpf_probe_read_kernel(&daddr, sizeof(daddr), &rt->rt6i_dst.addr);
-	struct net *net_ptr = NULL;
-    bpf_core_read(&net_ptr, sizeof(net_ptr), &sk->__sk_common.skc_net.net);
-    if (net_ptr) {
-        bpf_core_read(&net_ns_inum, sizeof(net_ns_inum), &net_ptr->ns.inum);
-	}
 #endif
 
 	tuple->saddr_h = saddr_h;
@@ -143,7 +144,7 @@ static int read_ipv6_tuple(struct ipv6_tuple_t *tuple, struct guess_status_t *st
 	tuple->daddr_l = daddr_l;
 	tuple->sport = sport;
 	tuple->dport = ntohs(dport);
-	tuple->netns = net_ns_inum;
+	tuple->netns = get_ns(status, sk);
 
 	// if addresses or ports are 0, ignore
 	if (!(saddr_h || saddr_l) || !(daddr_h || daddr_l) || dport == 0 || sport == 0 ) {
