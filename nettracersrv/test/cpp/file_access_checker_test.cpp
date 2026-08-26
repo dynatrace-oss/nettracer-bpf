ca5b7d585101dd0c85e3881a3858d44245967338
Mariusz Legowski <mariusz.legowski@dynatrace.com>
2026-08-26 08:20:30 +0200

    [DTSEC-28149] Created additional Unit Tests.

--- nettracersrv/test/cpp/file_access_checker_test.cpp
+++ nettracersrv/test/cpp/file_access_checker_test.cpp
@@ -16,18 +16,17 @@ protected:
 		existingFile = testDir / "test_file.txt";
 		nonExistingFile = testDir / "ghost_file.txt";
 
-		
 		std::ofstream ofs(existingFile);
 		ofs << "Hello GTest";
 		ofs.close();
 
-		
 		checker = IFileAccessChecker::create();
 	}
 
 	void TearDown() override {
-		
-		fs::remove_all(testDir);
+		std::error_code ec;
+		fs::permissions(testDir, fs::perms::owner_all, fs::perm_options::add, ec);
+		fs::remove_all(testDir, ec); 
 	}
 
 	fs::path testDir;
@@ -36,9 +35,7 @@ protected:
 	std::unique_ptr<IFileAccessChecker> checker;
 };
 
-
-
-
+//	EXISTENCE TESTS (F_OK)
 TEST_F(FileAccessCheckerTest, FileExistsShouldReturnTrue) {
 	EXPECT_TRUE(checker->hasAccess(existingFile, AccessMode::Exists));
 }
@@ -47,7 +44,7 @@ TEST_F(FileAccessCheckerTest, NonExistingFileShouldReturnFalseForExists) {
 	EXPECT_FALSE(checker->hasAccess(nonExistingFile, AccessMode::Exists));
 }
 
-
+//	READING TESTS (R_OK)
 TEST_F(FileAccessCheckerTest, RegularFileShouldBeReadable) {
 	EXPECT_TRUE(checker->hasAccess(existingFile, AccessMode::Read));
 }
@@ -56,6 +53,7 @@ TEST_F(FileAccessCheckerTest, NonExistingFileShouldNotBeReadable) {
 	EXPECT_FALSE(checker->hasAccess(nonExistingFile, AccessMode::Read));
 }
 
+//	WRITE TESTS (W_OK)
 TEST_F(FileAccessCheckerTest, RegularFileInTempDirShouldBeWritable) {
 	EXPECT_TRUE(checker->hasAccess(existingFile, AccessMode::Write));
 	EXPECT_TRUE(checker->hasAccess(existingFile, AccessMode::ReadWrite));
@@ -64,8 +62,6 @@ TEST_F(FileAccessCheckerTest, RegularFileInTempDirShouldBeWritable) {
 TEST_F(FileAccessCheckerTest, ProtectedFileShouldReturnFalseForWrite) {
 	fs::path protectedFile = testDir / "protected.txt";
 	std::ofstream(protectedFile).close();
-
-	
 	fs::permissions(protectedFile, fs::perms::owner_read, fs::perm_options::replace);
 
 	if (::getuid() != 0) {
@@ -74,6 +70,42 @@ TEST_F(FileAccessCheckerTest, ProtectedFileShouldReturnFalseForWrite) {
 	}
 }
 
+//	COMPOUND TESTS (R_OK | W_OK)
+TEST_F(FileAccessCheckerTest, RegularFileShouldBeReadWritable) {
+	EXPECT_TRUE(checker->hasAccess(existingFile, AccessMode::ReadWrite));
+}
+
+TEST_F(FileAccessCheckerTest, ReadOnlyFileShouldReturnFalseForReadWrite) {
+	fs::path readOnlyFile = testDir / "readonly.txt";
+	std::ofstream(readOnlyFile).close();
+	fs::permissions(readOnlyFile, fs::perms::owner_read, fs::perm_options::replace);
+
+	if (::getuid() != 0) {
+		EXPECT_FALSE(checker->hasAccess(readOnlyFile, AccessMode::ReadWrite));
+	}
+}
+
+// EXECUTION TESTS (X_OK)
+TEST_F(FileAccessCheckerTest, ExecutableFileShouldReturnTrueForExecute) {
+	fs::path execFile = testDir / "exec_file.sh";
+	std::ofstream(execFile).close();
+	fs::permissions(execFile, fs::perms::owner_exec, fs::perm_options::replace);
+
+	EXPECT_TRUE(checker->hasAccess(execFile, AccessMode::Execute));
+}
+
+TEST_F(FileAccessCheckerTest, NonExecutableFileShouldReturnFalseForExecute) {
+	fs::permissions(existingFile, fs::perms::none, fs::perm_options::replace);
+
+	if (::getuid() != 0) {
+		EXPECT_FALSE(checker->hasAccess(existingFile, AccessMode::Execute));
+	}
+}
+
+//  EDGE CASE TESTS AND ERRORS
+TEST_F(FileAccessCheckerTest, EmptyPathShouldReturnFalse) {
+	EXPECT_FALSE(checker->hasAccess(fs::path(""), AccessMode::Exists));
+}
 
 TEST_F(FileAccessCheckerTest, InvalidEnumModeShouldBeCaughtAndReturnFalse) {
 	auto corruptedMode = static_cast<AccessMode>(99);
